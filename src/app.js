3fc579ff2eeaa18bdb1510f3c4821b7b9491e56e
Jaegar Sarauer <jaegars@hotmail.com>
2015-05-04 21:38:35 -0700

    Not working grid
    its not working

--- src/app.js
+++ src/app.js
@@ -1,22 +1,70 @@
+/*used to dynamically calculate the grid sizes.*/
+var window = cc.winSize;
+
+/*The amount of cells in row */
+var cellsRow = 7;
+
+/*The amount of cells in a column */
+var cellsColumn = 9;
+
+/* the pixel width of a cell*/
+var cellWidth = window.width / cellsRow;
+
+/* the pixel height of a cell */
+var cellHeight = window.height / cellsColumn;
+
+/*The amount of cells in the entire grid */
+var gridCells = cellsRow * cellsColumn;
+
+/*The object, cell. Contains data for a cell.
+		Used as a spot in the grid. */
+var cell= {
+		isEmpty: true,
+		containsID: 0
+};
+
+/* Creates a grid of cells based off of varaibles above. */
+function createGrid() {
+	var grid = [];
+	for (i = 0; i < cellsColumn; i++) {
+		grid[i] = new Array(cellsRow);
+		for (j = 0; j < cellsRow; j++) {
+			grid[i][j] = cell;
+		}
+	}
+	return grid;
+};
+
+/* Gets the canvas to draw to. */
+function getCanvas() {
+	var c = document.getElementById("gameCanvas");
+	var ctx = c.getContext("2d");
+	return ctx;
+};
+
+
+
+
 var GameLayer = cc.Layer.extend({
-	sprite:null,
+	grid:null,
+	
+	
+	initGrid:function() {
+		grid = createGrid();
+		var draw = cc.DrawNode.create();
+		this.addChild(this.draw, 100);
+		for (i = 0; i < gridCells; i++) {
+			this.draw.drawRect(cc.p(i % cellsRow, Math.floor(i / cellsColumn)), cc.p(i % cellsRow + cellWidth, Math.floor(i / cellsColumn) + cellHeight), cc.Color(255,255,255,1), 4, cc.Color(255,255,255,1));
+		}
+	},
 	ctor:function () {
-		//////////////////////////////
-		// 1. super init first
 		this._super();
+		this.initGrid();
 
-		var size = cc.winSize;
-
-		var sprite = new cc.Sprite.create(res.CloseNormal_png);
-		sprite.setAnchorPoint(cc.p(0.5, 0.5));
-		sprite.setPosition(cc.p(size.width/2, size.height/2));
-		this.addChild(sprite, 0);
-		
-		var sprite_action = cc.MoveTo.create(2, cc.p(50, 100));
-		sprite.runAction(sprite_action);
-		
 		return true;
 	}
+
+
 });
 
 var GameScene = cc.Scene.extend({
@@ -26,3 +74,4 @@ var GameScene = cc.Scene.extend({
 		this.addChild(layer);
 	}
 });
+
