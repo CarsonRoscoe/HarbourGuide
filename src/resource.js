9e7bae66613fbe09dd44246c374253a56e6bb9c3
Eric Lui <i-meric@hotmail.com>
2015-05-08 03:36:57 -0700

    Added sound files

--- src/resource.js
+++ src/resource.js
@@ -12,6 +12,9 @@ var res = {
     ObstacleSmall_png : folder + "ObstacleSmall.png",
     ObstacleMedium_png : folder + "ObstacleMedium.png",
     ObstacleLarge_png : folder + "ObstacleLarge.png"
+    background_mp3 : "res/sound/mainbg.mp3",
+    button : "res/sound/button.mp3",
+    GameBackground_mp3 : "res/sound/gamebg.mp3"
 };
 
 var g_resources = [];
