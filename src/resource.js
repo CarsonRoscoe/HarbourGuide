3045cb8ccb540d2bb1494b4c4219bc43682f5554
Eric Lui <i-meric@hotmail.com>
2015-05-10 02:33:06 -0700

    Added button to resources

--- src/resource.js
+++ src/resource.js
@@ -14,7 +14,8 @@ var res = {
     ObstacleLarge_png : folder + "ObstacleLarge.png",
     background_mp3 : "res/sound/mainbg.mp3",
     button : "res/sound/button.mp3",
-    GameBackground_mp3 : "res/sound/gamebg.mp3"
+    GameBackground_mp3 : "res/sound/gamebg.mp3",
+    Button_png : "res/pause.png",
 };
 
 var g_resources = [];
