f4d6d6b2a218aad4ad76537b7bf5fb7da44cd5ee
Carson Roscoe <CarsonRoscoe7@gmail.com>
2015-05-17 16:43:45 -0700

    Added AES Encryption Client-Side
    All data request to the server are now encrypted using AES encryption.

--- src/connection.js
+++ src/connection.js
@@ -6,6 +6,8 @@ var test = function() {
 };
 
 var sendCommand = function(command, dP) {
+	var key = CryptoJS.enc.Utf8.parse('8080808080808080');
+	var iv = CryptoJS.enc.Utf8.parse('8080808080808080');
 	var formatted = "";
 	switch(command) {
 	case "GETSCORE;":
@@ -19,13 +21,28 @@ var sendCommand = function(command, dP) {
 		return false;
 	break;
 	}
+	var enc = encryptData(formatted, key, iv);
 	$.ajax({
 		type:"GET",
-		url: "http://52.11.0.80:8080/" + formatted,
+		url: "http://52.11.0.80:8080/" + enc,
 		crossDomain: true,
 		dataType: 'jsonp'
 	});
 };
+
+var encryptData = function(formatted, key, iv) {
+	var str = formatted;
+	var encryptedString = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(str), key, {
+		keySize: 128/8,
+		iv: iv,
+		mode: CryptoJS.mode.CBC,
+		padding: CryptoJS.pad.Pkcs7
+	});
+	var str2 = "[[";
+	str2 += encryptedString;
+	str2 += "]]";
+	return str2;
+}
 
 var getDataPackArray = function() {
 	return dataPackArray;
