970ca514960c88f313ec3dab22cba94badbb533c
Eric Lui <i-meric@hotmail.com>
2015-05-21 22:20:15 -0700

    Almost done, this is to save a copy that works
    Might break this in a few seconds.

--- src/SettingsScene.js
+++ src/SettingsScene.js
@@ -22,20 +22,49 @@ var SettingsLayer = cc.Layer.extend({
 		Layer.currentEffectVolume = cc.audioEngine.getEffectsVolume().toFixed(1);
 
 
-		//Static right now because its giving me hell, not sure what else I can do.
-		bgVolumeLabel = new cc.LabelTTF("Main Volume: " + Layer.currentVolume, "Helvetica", 30);
-		bgVolumeLabel.setColor(cc.color(200,200,200));
-		bgVolumeLabel.setPosition(cc.p(winsize.width / 2, winsize.height * 0.75));
-		Layer.addChild(bgVolumeLabel);
+		var musicVolumeLabel = new cc.LabelTTF("Background Music Volume", "Helvetica", 30);
+		
+		bgVolumeLabel = new cc.LabelTTF(Layer.currentVolume, "Helvetica", 30);
+		bgVolumeLabel.setColor(cc.color(200,200,200));	
+		
+		var musicUp = new cc.MenuItemSprite(
+				new cc.Sprite("res/mediumRes/buttons/volume_up_default.png"),
+				new cc.Sprite("res/mediumRes/buttons/volume_up_pressed.png"),
+				Layer.volumeUp, this);
+		var menuItem2 = new cc.MenuItemLabel(bgVolumeLabel, Layer.backScene);
+		var musicDown = new cc.MenuItemSprite(
+				new cc.Sprite("res/mediumRes/buttons/volume_down_default.png"),
+				new cc.Sprite("res/mediumRes/buttons/volume_down_pressed.png"),
+				Layer.volumeDown, this);
+		var musicMenu = new cc.Menu(musicUp, menuItem2, musicDown);
+		musicMenu.alignItemsHorizontallyWithPadding(50);
+		musicMenu.setPosition(cc.p(winsize.width/2, winsize.height/1.5));
+		Layer.addChild(musicMenu);
 
-		effectVolumeLabel = new cc.LabelTTF("Effect Volume: " + Layer.currentEffectVolume, "Helvetica", 30);
+		
+		effectVolumeLabel = new cc.LabelTTF(Layer.currentEffectVolume, "Helvetica", 30);
 		effectVolumeLabel.setColor(cc.color(200,200,200));
-		effectVolumeLabel.setPosition(cc.p(winsize.width / 2, winsize.height * 0.69));
-		Layer.addChild(effectVolumeLabel);
+		
+		var effectUp = new cc.MenuItemSprite(
+				new cc.Sprite("res/mediumRes/buttons/volume_up_default.png"),
+				new cc.Sprite("res/mediumRes/buttons/volume_up_pressed.png"),
+				Layer.volumeEffectUp, this);
+		var menuItem3 = new cc.MenuItemLabel(effectVolumeLabel, Layer.backScene);
+		var effectDown = new cc.MenuItemSprite(
+				new cc.Sprite("res/mediumRes/buttons/volume_down_default.png"),
+				new cc.Sprite("res/mediumRes/buttons/volume_down_pressed.png"),
+				Layer.volumeEffectDown, this);
+		var effectMenu = new cc.Menu(effectUp, menuItem3, effectDown);
+		effectMenu.alignItemsHorizontallyWithPadding(50);
+		effectMenu.setPosition(cc.p(winsize.width/2, winsize.height/3));
+		Layer.addChild(effectMenu);
+		
+		
 
 		//MenuItem to navigate to MenuScene
 		//Settings is a placeholder, not clickable
 		//Volumes are backwards here but is drawn correctly in game
+		/*
 		var menuItem1 = new cc.MenuItemFont("Settings");
 		var menuItem2 = new cc.MenuItemFont("Back", Layer.backScene);
 		var menuItem3 = new cc.MenuItemFont("Game Volume Down", Layer.volumeDown);
@@ -43,14 +72,14 @@ var SettingsLayer = cc.Layer.extend({
 		var menuItem5 = new cc.MenuItemFont("Effect Volume Down", Layer.volumeEffectDown);
 		var menuItem6 = new cc.MenuItemFont("Effect Volume Up", Layer.volumeEffectUp);
 		colorBlind = new cc.MenuItemFont("Color Blind Mode", Layer.changeThing);
-
+		*/
 		//Adds menuItems to a Menu. The volume up and down's are backwards because it was running the functions in them on loading.
 		//So now it subtracts then adds to the volume very quickly so that you won't notice.
-		var menu = new cc.Menu(menuItem1, menuItem2, menuItem4, menuItem3, menuItem6, menuItem5, colorBlind);
+		//var menu = new cc.Menu(menuItem1, menuItem2, menuItem4, menuItem3, menuItem6, menuItem5, colorBlind);
 		//Aligns the items vertically
-		menu.alignItemsVertically();
+		//menu.alignItemsVertically();
 		//Adds menu to layer
-		Layer.addChild(menu);
+		//Layer.addChild(menu);
 
 	},
 	
@@ -62,8 +91,8 @@ var SettingsLayer = cc.Layer.extend({
 		if(display <= 0){
 			display = 0;
 		}
-		bgVolumeLabel.setString("Main Volume: " + display.toFixed(1));
-		
+		bgVolumeLabel.setString(display.toFixed(1));
+
 	},
 
 	//The volume up and down functions adjust the background music by increments of 10%. Starting volume is 100%
@@ -74,10 +103,10 @@ var SettingsLayer = cc.Layer.extend({
 		if(display >= 1){
 			display = 1;
 		}
-		bgVolumeLabel.setString("Main Volume: " + display.toFixed(1));
-		
+		bgVolumeLabel.setString(display.toFixed(1));
+
 	},
-	
+
 	volumeEffectDown: function() {
 		var current = cc.audioEngine.getEffectsVolume() - 0.1;
 		cc.audioEngine.setEffectsVolume(current);
@@ -85,8 +114,8 @@ var SettingsLayer = cc.Layer.extend({
 		if(display <= 0){
 			display = 0;
 		}
-		effectVolumeLabel.setString("Effect Volume: " + display.toFixed(1));
-		
+		effectVolumeLabel.setString(display.toFixed(1));
+
 	},
 
 	//The volume up and down functions adjust the   background music by increments of 10%. Starting volume is 100%
@@ -97,8 +126,8 @@ var SettingsLayer = cc.Layer.extend({
 		if(display >= 1){
 			display = 1;
 		}
-		effectVolumeLabel.setString("Effect Volume: " + display.toFixed(1));
-		
+		effectVolumeLabel.setString(display.toFixed(1));
+
 	},
 	
 	//The following function is called when the button in the menu is pressed
@@ -109,10 +138,12 @@ var SettingsLayer = cc.Layer.extend({
 		cc.director.popScene();
 	},
 	
+	/*
 	//Just testing things with this
 	changeThing: function() {
 		colorBlind.setString("This doesn't work yet.");
 	}
+	*/
 });
 
 
